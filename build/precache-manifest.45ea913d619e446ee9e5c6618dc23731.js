04fce76f12d85ed53f82cda303f2cbeaba9596eb
Agustin <agusgarba@gmail.com>
2019-02-02 23:39:19 -0300

    updates to send files to heroku

---
+++ build/precache-manifest.45ea913d619e446ee9e5c6618dc23731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "521adf2284442e52aa8b",
+    "url": "/static/js/main.521adf22.chunk.js"
+  },
+  {
+    "revision": "b546083f1c404acd1435",
+    "url": "/static/js/1.b546083f.chunk.js"
+  },
+  {
+    "revision": "521adf2284442e52aa8b",
+    "url": "/static/css/main.1b6233ad.chunk.css"
+  },
+  {
+    "revision": "6dbe9e5cbc983b9464ecdde8373907ce",
+    "url": "/index.html"
+  }
+];
